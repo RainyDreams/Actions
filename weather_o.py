754cea153289a2b77e5bd337214e04374b66776b
RainyDreams <141991010+RainyDreams@users.noreply.github.com>
2024-01-31 20:28:16 +0800

    test

--- weather_o.py
+++ weather_o.py
@@ -6,16 +6,20 @@ from bs4 import BeautifulSoup
 # 从测试号信息获取
 appID = os.environ.get("APP_ID")
 appSecret = os.environ.get("APP_SECRET")
-# 收信人ID即 用户列表中的微信号
 openId_str = os.environ.get("OPEN_ID")
-# 天气预报模板ID
 weather_template_id = os.environ.get("TEMPLATE_ID_OGIMET")
 uid = weather_template_id.strip() or "ljryQcnRHQXQGWUGv8i8zoGsOp4AUKpYiiHa28eVSDQ"
-
-# 将OPEN_ID字符串转换为数组
 openId_list = openId_str.split(",")
 
-def get_third_row(url):
+from datetime import date
+today = date.today()
+year = today.year
+month = today.month
+day = today.day
+url = 'http://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=54218&ndays=1&ano={}&mes={}&day={}&hora=00&ord=REV&Send=Send'\
+    .format(year,month,day)
+
+def get_weather():
     # 发送GET请求获取网页代码
     response = requests.get(url)
     html = response.text
@@ -24,25 +28,14 @@ def get_third_row(url):
     # 找到所有的table元素
     tables = soup.find_all('table')
     if len(tables) < 3:
-        raise ValueError("网页中至少需要5个table元素")
+        raise ValueError("网页中至少需要3个table元素")
     target_table = tables[2]
     # 找到第三行(tr)
     third_row = target_table.find_all('tr')[2]
     # 获取第三行的td数据，组成一个数组
     row_data = [cell.text.strip() for cell in third_row.find_all('td')]
+    print(row_data)
     return row_data
-
-def get_weather():
-    from datetime import date
-    today = date.today()
-    year = today.year
-    month = today.month
-    day = today.day
-    url = 'https://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=54218&ndays=1&ano={}&mes={}&day={}&hora=00&ord=REV&Send=Send'\
-        .format(year,month,day)
-    data = get_third_row(url)
-    # print(url)
-    return data
 
 
 def get_access_token():
@@ -55,13 +48,6 @@ def get_access_token():
     return access_token
 
 def send_weather(access_token, weather, openId):
-    from datetime import date
-    today = date.today()
-    year = today.year
-    month = today.month
-    day = today.day
-    url = 'https://www.ogimet.com/cgi-bin/gsynres?lang=en&ind=54218&ndays=1&ano={}&mes={}&day={}&hora=00&ord=REV&Send=Send'\
-        .format(year,month,day)
     body = {
         "touser": openId.strip(),
         "template_id": uid,
@@ -113,7 +99,7 @@ def weather_report():
     access_token = get_access_token()
     # 获取天气
     weather = get_weather()
-    print(f"天气信息： {weather}")
+    print(weather)
     # 逐个推送消息
     for openId in openId_list:
         send_weather(access_token, weather, openId)
